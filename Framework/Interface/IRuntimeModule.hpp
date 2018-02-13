45577ae67202d6eac5f74cd5a6d7967d0c312574
CanftIn <wwc7033@gmail.com>
2018-02-13 15:29:43 +0800

    remake project structure

---
+++ Framework/Interface/IRuntimeModule.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "Interface.hpp"
+
+namespace My {
+	Interface IRuntimeModule{
+public:
+	virtual ~IRuntimeModule() {};
+
+	virtual int Initialize() = 0;
+	virtual void Finalize() = 0;
+
+	virtual void Tick() = 0;
+	};
+
+}
